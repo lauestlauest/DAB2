c0836df387ef84f4342457128e0d00a476b3138b
lauestlauest <76699249+lauestlauest@users.noreply.github.com>
2023-04-05 21:14:57 +0200

    first draft

---
+++ DABAssignment2/DABAssignment2/Model/Canteens.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DABAssignment2.Model
+{
+    public class Canteens
+    {
+        //Table Elements
+        [Key]
+        public string CanteenName;
+
+        public string WarmMealName;
+
+        public string StreetMealName;
+
+        public int AmountWarmMeal;
+
+        public int AmountStreetMeal;
+
+        public int PostCode;
+
+
+        //Relations
+        public ICollection<JITItems> JitItems { get; }
+
+        public ICollection<Reservations> Reservations { get; }
+        
+        public ICollection<Ratings> Ratings { get; }
+    }
+}
