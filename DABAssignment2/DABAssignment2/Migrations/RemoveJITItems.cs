e7c42d869da29031d52f36315f39d0f93eb95909
lauestlauest <76699249+lauestlauest@users.noreply.github.com>
2023-04-06 22:26:11 +0200

    more changes

--- DABAssignment2/DABAssignment2/Migrations/RemoveJITItems.cs
+++ DABAssignment2/DABAssignment2/Migrations/RemoveJITItems.cs
@@ -7,31 +7,13 @@ namespace DABAssignment2.Migrations
     public partial class RemoveJITItemsTable : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
-        {  migrationBuilder.DropTable("JITItems");
+        {
+            throw new NotImplementedException();
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                    name: "JITItems",
-                    columns: table => new
-                    {
-                        JITItemsId = table.Column<int>(type: "int", nullable: false)
-                            .Annotation("SqlServer:Identity", "1, 1"),
-                        JITMealName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                        CanteenName = table.Column<string>(type: "nvarchar(450)", nullable: false)
-                    },
-                    constraints: table =>
-                    {
-                        table.PrimaryKey("PK_JITItems", x => x.JITItemsId);
-                        table.ForeignKey(
-                            name: "FK_JITItems_Canteens_CanteenName",
-                            column: x => x.CanteenName,
-                            principalTable: "Canteens",
-                            principalColumn: "CanteenName",
-                            onDelete: ReferentialAction.Cascade);
-                    });
-            
+            throw new NotImplementedException();
         }
     }
 }
--- DABAssignment2/DABAssignment2/Program.cs
+++ DABAssignment2/DABAssignment2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DABAssignment2.Data;
+using DABAssignment2.Model;
 
 namespace DABAssignment2
 {
@@ -6,8 +7,60 @@ namespace DABAssignment2
     {
         static void Main(string[] args)
         {
-            using var db = new AUCanteens();
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("Hello, DAB!");
+
+            Console.WriteLine("Inserting Dummy data...");
+
+            using (var db = new AUCanteens())
+            {
+                //Canteens
+                db.Canteens.Add(new Canteens { CanteenName = "Kgl. Bibliotek", PostCode = 8000 });
+                db.Canteens.Add(new Canteens { CanteenName = "Kemisk", PostCode = 8300 });
+                db.Canteens.Add(new Canteens { CanteenName = "Matematisk", PostCode = 8000 });
+
+                //Menu
+                db.Menu.Add(new Menu { MenuItemsId = 1, MealType = 0, MealName = "æg", CanteenName = "Kgl. Bibliotek" });
+                db.Menu.Add(new Menu { MenuItemsId = 2, MealType = 0, MealName = "æg", CanteenName = "Kgl. Bibliotek" });
+                db.Menu.Add(new Menu { MenuItemsId = 3, MealType = 0, MealName = "æg", CanteenName = "Kgl. Bibliotek" });
+                db.Menu.Add(new Menu { MenuItemsId = 4, MealType = 0, MealName = "æg", CanteenName = "Kemisk" });
+                db.Menu.Add(new Menu { MenuItemsId = 5, MealType = 0, MealName = "æg", CanteenName = "Kemisk" });
+                db.Menu.Add(new Menu { MenuItemsId = 6, MealType = 0, MealName = "æg", CanteenName = "Kemisk" });
+                db.Menu.Add(new Menu { MenuItemsId = 7, MealType = 0, MealName = "æg", CanteenName = "Matematisk" });
+                db.Menu.Add(new Menu { MenuItemsId = 8, MealType = 0, MealName = "æg", CanteenName = "Matematisk" });
+
+                //Customer 
+                db.Customer.Add( new Customer { CustomerCPR = 1400 });
+                db.Customer.Add(new Customer { CustomerCPR = 1500 });
+                db.Customer.Add(new Customer { CustomerCPR = 1300 });
+                db.Customer.Add(new Customer { CustomerCPR = 1200 });
+
+                //Reservations 
+                db.Reservations.Add(new Reservations { MealId = 0, CustomerCPR = 1400, MenuItemId = 1, CanteenName = "Kgl.Bibliotek" });
+                db.Reservations.Add(new Reservations { MealId = 1, CustomerCPR = 1500, MenuItemId = 1, CanteenName = "Kgl.Bibliotek" });
+                db.Reservations.Add(new Reservations { MealId = 2, CustomerCPR = 1300, MenuItemId = 1, CanteenName = "Kgl.Bibliotek" });
+                db.Reservations.Add(new Reservations { MealId = 3, CustomerCPR = 1200, MenuItemId = 1, CanteenName = "Kgl.Bibliotek" });
+
+                //Ratings
+                db.Ratings.Add(new Ratings {RatingsId = 0, CanteenName = "Kgl.Bibliotek" , CustomerCPR = 
+                    1400 });
+                db.Ratings.Add(new Ratings { RatingsId = 1, CanteenName = "Kgl.Bibliotek", CustomerCPR = 1400 });
+                db.Ratings.Add(new Ratings { RatingsId = 2, CanteenName = "Kgl.Bibliotek", CustomerCPR = 1400 });
+                db.Ratings.Add(new Ratings { RatingsId = 3, CanteenName = "Kgl.Bibliotek" , CustomerCPR = 1400 });
+
+                db.SaveChanges();
+
+            }
+
+
+            Console.WriteLine("Query 1");
+            Console.WriteLine("Query 2");
+            Console.WriteLine("Query 3");
+            Console.WriteLine("Query 4");
+            Console.WriteLine("Query 5");
+            Console.WriteLine("Query 6");
+
+            Console.WriteLine("Hello, DAB!");
         }
     }
 }
